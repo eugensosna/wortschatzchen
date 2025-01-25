730d8dc48ed8eb5d1cd04898fe38c34beeba4eac
Yevhen Sosna <yevhensosna@gmx.de>
2025-01-25 21:43:52 +0100

    init

---
+++ src/main/java/ua/sosna/wortschatz/wortschatztchen/WortschatztchenApplication.java
@@ -0,0 +1,13 @@
+package ua.sosna.wortschatz.wortschatztchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WortschatztchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WortschatztchenApplication.class, args);
+	}
+
+}
