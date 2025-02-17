825abd880e03888d3aa54481194f0fbf9c807008
Yevhen Sosna <yevhensosna@gmx.de>
2025-02-17 21:30:14 +0100

    fix naming

--- src/main/java/ua/sosna/wortschatz/wortschatztchen/dto/LingvanexTranslateDto.java
+++ src/main/java/ua/sosna/wortschatz/wortschatztchen/dto/LingvanexTranslateDto.java
@@ -3,10 +3,10 @@ package ua.sosna.wortschatz.wortschatztchen.dto;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LingvanexTranslate {
+public class LingvanexTranslateDto {
 	String platform = "api";
 	String from;
 	String  to;
-	List<String> data = new ArrayList<>();
+	String data = "";
 
 }
